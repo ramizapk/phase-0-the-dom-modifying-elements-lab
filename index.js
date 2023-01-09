e38ef20ca651f674bdeed41f753e577d58577702
ramiz ali <ramiz.apk7@gmail.com>
2023-01-10 01:36:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent="Ramiz is the champion";
